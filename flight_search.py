f6e6913ff5169760d65279fd0fcd66f3b1156545
Theophilus Asamoah Gyapong <jhapson34@gmail.com>
2025-06-16 20:19:06 +0000

    make few changes

--- flight_search.py
+++ flight_search.py
@@ -3,11 +3,6 @@ from datetime import datetime, timedelta
 
 import requests
 
-from sheety_data import SheetyData
-
-sheety_data = SheetyData()
-sheety_data.get_and_update_iataCodes()  # update iata codes in the sheety rows
-
 url = 'https://test.api.amadeus.com/v1/security/oauth2/token'
 
 
@@ -30,12 +25,9 @@ six_months_later = tomorrow + timedelta(days=6*30)  # Approximation of 6 months
 date_list = [tomorrow + timedelta(days=i)
              for i in range((six_months_later - tomorrow).days + 1)]
 
-for date in date_list:
-    date.strftime('%Y-%m-%d')
-
 
 class FlightSearch:
-    def __init__(self, destination_code,):
+    def __init__(self, destination_code, preferred_price: float):
         self.URL = 'https://test.api.amadeus.com/v2/shopping/flight-offers'
         self.origin_code = "KMS"  # Ksi airport code
         self.destination_code = destination_code
@@ -43,20 +35,32 @@ class FlightSearch:
         self.header = {
             'authorization': f"Bearer {os.environ.get('flight_token')}",
         }
+        self.preferred_price = preferred_price
+        self.flight_search_parameters: dict = {}
+        self.flight_offers_data = list
+        self.get_parameter_date()
+        self.message: str = ''
 
-    def get_parameter_date(self, dateList: list[datetime]):
+    def get_parameter_date(self,):
         """Convert a list of datetime objects to a list of strings in 'YYYY-MM-DD' format.
-
+            Gets flight offers data for a list of dates.
         """
-        for date in dateList:
+        for date in self.from_date:
             if not isinstance(date, datetime):
                 raise ValueError(
-                    "All items in dateList must be datetime objects.")
-            if not dateList:
-                raise ValueError("dateList cannot be empty.")
+                    "All items in self.from_date must be datetime objects.")
+            if not self.from_date:
+                raise ValueError("self.from_date cannot be empty.")
 
             self.flight_search_parameters = {'originLocationCode': self.origin_code, 'destinationLocationCode': self.destination_code,
                                              'departureDate': date.strftime('%Y-%m-%d'), 'adults': 1, 'currencyCode': 'GHS', 'max': 5, }
             flight_response = requests.get(
                 url=self.URL, headers=self.header, params=self.flight_search_parameters)
             flight_response.raise_for_status()
+            self.flight_offers_data = flight_response.json()['data']
+
+            # check if flight offer price meet or is less than preferred price
+            for flight_dict in self.flight_offers_data:
+                if int(flight_dict['price']['total']) <= self.preferred_price:
+                    self.message += f"On {(flight_dict['itineraries'][0]['segments'][0]['departure']['at']).strip('T')},\nthere is flight moving from Kumasi to {self.destination_code}\nat a price of GHS {flight_dict['price']['total']}\nwhich meet the preferred price requirement.\n"
+                    return self.message
