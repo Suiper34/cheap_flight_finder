6c3ebc434b929ab38f19f04812deda9598ce15b8
Theophilus Asamoah Gyapong <jhapson34@gmail.com>
2025-06-17 18:35:46 +0000

    check for direct flights, if it returns no flights then make second request to check for transits. Alert user with messages on both cases if true

--- flight_search.py
+++ flight_search.py
@@ -20,7 +20,7 @@ today = datetime.now()
 
 tomorrow = today + timedelta(days=1)  # tomorrow's date
 
-six_months_later = tomorrow + timedelta(days=3*30)  # Approximation of 3 months
+six_months_later = tomorrow + timedelta(days=6*30)  # Approximation of 6 months
 
 # Generate a list of dates from tomorrow to six months later
 date_list = [tomorrow + timedelta(days=i)
@@ -40,6 +40,7 @@ class FlightSearch:
         self.flight_search_parameters: dict = {}
         self.flight_offers_data: list = []
         self.message: str = ''
+
         self.get_parameter_date()
 
     def get_parameter_date(self,) -> str:
@@ -49,17 +50,36 @@ class FlightSearch:
         for date in self.from_date:
             if not isinstance(date, datetime):
                 raise ValueError(
-                    "All items in self.from_date must be datetime objects.")
+                    "All items in self.from_date(DateList) must be datetime objects.")
             if not self.from_date:
-                raise ValueError("self.from_date cannot be empty.")
+                raise ValueError("self.from_date(DateList) cannot be empty.")
 
+            self.non_stop = True
             self.flight_search_parameters = {'originLocationCode': self.origin_code, 'destinationLocationCode': self.destination_code,
-                                             'departureDate': date.strftime('%Y-%m-%d'), 'adults': 1, 'currencyCode': 'GHS', 'max': 5, }
+                                             'departureDate': date.strftime('%Y-%m-%d'), 'adults': 1, 'currencyCode': 'GHS', 'max': 5, 'nonStop': self.non_stop, }
             try:
+                # make a request to the flight offers API
                 flight_response = requests.get(
                     url=self.URL, headers=self.header, params=self.flight_search_parameters)
                 flight_response.raise_for_status()
                 data = flight_response.json().get('data', [])
+
+                # notify user as to whether it is direct flight or transit
+                if data:
+
+                    self.message += f'\nThis is a straight flight to {self.destination_code} from Kumasi.\n'
+                else:
+
+                    self.non_stop = False  # if the request fails, set non-stop to false
+                    self.flight_search_parameters['nonStop'] = self.non_stop
+                    flight_response = requests.get(
+                        url=self.URL, headers=self.header, params=self.flight_search_parameters)
+                    data = flight_response.json().get('data', [])
+                    if data:
+                        self.message += '\nThere will be transits with this flight.\n'
+                    else:
+                        continue  # No flights at all for this date, skips to next date
+
             except Exception as e:
                 print(f"Error fetching flights for {date}: {e}")
                 continue
@@ -79,7 +99,7 @@ class FlightSearch:
                             0]  # Splits the datetime string at the 'T' and takes the date part
                     except (KeyError, IndexError):
                         flight_date = 'Unknown date'
-                    self.message += f"On {flight_date},\nthere is flight moving from Kumasi to {self.destination_code}\nat a price of GHS {flight_dict['price']['total']}\nwhich meet the preferred price requirement.\n"
+                    self.message += f"\nOn {flight_date},\nthere is flight moving from Kumasi to {self.destination_code}\nat a price of GHS {flight_dict['price']['total']}\nwhich meet the preferred price requirement.\n\n"
 
             sleep(1.5)  # delay 1.5secs to avoid hitting API rate limits
 
