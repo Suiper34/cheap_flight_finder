145ad7bdfabbe661521b4462bb54a19e4899842f
Theophilus Asamoah Gyapong <jhapson34@gmail.com>
2025-06-17 14:00:37 +0000

    limit the number of messages sent via whatsapp

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from flight_search import FlightSearch
 from sheety_data import SheetyData
 from twilio_whatsapp import SendEmail, TwilioWhatsApp
 
+MAX_MESSAGES = 10
+sent_count = 0
+
 sheety_data = SheetyData()
 sheety_data.get_and_update_iataCodes()  # update iata codes in the sheety rows
 
@@ -35,11 +38,19 @@ for flight_offer in flight_deal_data:
         print(f'No message generated for {iata_code}')
         continue
 
-    twilio_whatsapp = TwilioWhatsApp()
-    try:
-        twilio_whatsapp.send_whatsapp_message(flight_search.message)
-    except Exception as e:
-        print(f'Failed to send WhatsApp message: {e}')
+    # allow not more than 10 messages to be sent
+    if flight_search.message:
+        if sent_count >= MAX_MESSAGES:
+            print("Reached message sending limit.")
+            break
+        twilio_whatsapp = TwilioWhatsApp()
+        try:
+            twilio_whatsapp.send_whatsapp_message(flight_search.message)
+            sent_count += 1
+        except Exception as e:
+            print(f'Failed to send WhatsApp message: {e}')
+
+    # send via message email
     send_mail = SendEmail()
     try:
         send_mail.send_email(flight_search.message)
